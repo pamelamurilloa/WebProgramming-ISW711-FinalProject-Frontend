976c36c7032cb520b27e2c0912268e958519b5e4
Pamela Murillo <pamelamurilloa@gmail.com>
2024-04-23 11:54:27 -0600

    Advancing on integrating twilio in the frontend

--- src/components/organisms/LoginForm.jsx
+++ src/components/organisms/LoginForm.jsx
@@ -6,12 +6,15 @@ import Input from '../atoms/Input'
 import Submit from '../atoms/Submit'
 import FrontPageLayout from '../layouts/FrontPageLayout/FrontPageLayout'
 import { useAuth } from '../../contexts/authContext'
-import { useSession } from '../../../hooks/useSession'
+import { useSession } from '../../../hooks/users/useSession'
 
 const LoginForm = () => {
 
-    const {user, setUser} = useAuth();
-    const {loading, data, isError, login} = useSession();
+    const {user, setUser} = useAuth()
+    const {loading, data, isError, login} = useSession()
+
+    const [code, setCode] = useState('')
+    const [isCodeSent, setIsCodeSent] = useState('')
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -28,12 +31,16 @@ const LoginForm = () => {
     const handleLogin = (event) => {
         event.preventDefault()
 
-        login(email, password);
+        login(email, password)
 
 
         setUser({name:'Pam', lastName:'Murillo'});
         console.log(`You've logged in with email: ${email}, password: ${password}`)
         //message with backend
+    }
+
+    const handleCodeSent = (event) => {
+        event.preventDefault()
     }
 
     return (
@@ -55,6 +62,21 @@ const LoginForm = () => {
                 {loading ? "Loading..." : <Submit />}
                 <Link className="main-button" to="/register">Register</Link>
             </form>
+
+            {
+                <PopUp>
+                    <h3>Enter the code sent to your cellphone</h3>
+                    <form id="login-form" onSubmit={handleCodeSent}>
+                        <Input
+                            id="code" placeholder="Your code" required
+                            maxLength="5" minLength="5"
+                            value={code} 
+                            onChange={setCode}
+                        />
+                        <Submit/>
+                    </form>
+                </PopUp>
+            }
         </FrontPageLayout>
     )
 }
